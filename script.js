c8253e6d989140bfd4fb55581a6137770848cc27
Angiephoenix <angiephoenix93@gmail.com>
2023-08-13 13:06:59 +0100

    updated skillset

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer"],
+        strings: ["Full-Stack Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true

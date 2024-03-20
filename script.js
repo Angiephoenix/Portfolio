175bb66ff96ab5644607693821e29b34f742fc90
Angiephoenix <angiephoenix93@gmail.com>
2024-03-20 15:26:10 +0100

    experience update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [" Software Engineer,", " Product Manager,", " DevRel."],
+        strings: [" Software Engineer,", " Technical Product Manager,", " DevRel."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [" Software Engineer,", " Product Manager,", " DevRel."],
+        strings: [" Software Engineer,", " Technical Product Manager,", " DevRel."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
